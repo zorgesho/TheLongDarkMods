1c96cc4957ab3814e468fd3e88fbdbeb04341779
zorgesho <53096970+zorgesho@users.noreply.github.com>
2021-04-13 19:11:44 +0300

    SkipIntro project added

---
+++ SkipIntro/properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SkipIntro")]
+[assembly: AssemblyProduct("SkipIntro")]
+[assembly: AssemblyCopyright("© 2021 zorgesho")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion(SkipIntro.Main.version)]
+
+[assembly: MelonInfo(typeof(SkipIntro.Main), "SkipIntro", SkipIntro.Main.version, "zorgesho")]
+[assembly: MelonGame("Hinterland", "TheLongDark")]
---
+++ SkipIntro/src/Patches.cs
@@ -0,0 +1,30 @@
+﻿using Harmony;
+using Common;
+
+namespace SkipIntro
+{
+	[PatchClass]
+	static class Patches
+	{
+		[HarmonyPostfix, HarmonyPatch(typeof(BootUpdate), "Update")]
+		static void BootUpdate_Update_Postfix(BootUpdate __instance) => __instance.m_Label_Continue.gameObject.SetActive(false);
+
+		[HarmonyPrefix, HarmonyPatch(typeof(Panel_MainMenu), "Enable")]
+		static void PanelMainMenu_Enable_Prefix(Panel_MainMenu __instance) => MoviePlayer.m_HasIntroPlayedForMainMenu = __instance.m_StartFadedOut = true;
+
+		[HarmonyPrefix, HarmonyPatch(typeof(Panel_MainMenu), "UpdateFading")]
+		static void PanelMainMenu_UpdateFading_Prefix(Panel_MainMenu __instance) => __instance.m_InitialScreenFadeInDuration = 0f;
+
+		[HarmonyPrefix, HarmonyPatch(typeof(Panel_MainMenu), "SetPanelAlpha")]
+		static void PanelMainMenu_SetPanelAlpha_Prefix(ref float alpha) => alpha = 1f;
+
+		[HarmonyPrefix, HarmonyPatch(typeof(BootUpdate), "Start")]
+		static void BootUpdate_Start_Prefix(BootUpdate __instance)
+		{
+			for (int i = 1; i <= 3; i++)
+				__instance.gameObject.getChild($"Label_Disclaimer_{i}").SetActive(false);
+
+			UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+		}
+	}
+}

43f05f402f9f6e2a964543b5b20ceae0ce7098c4
zorgesho <53096970+zorgesho@users.noreply.github.com>
2021-07-01 09:49:24 +0300

    PlaceFromInventory 1.1.0

--- PlaceFromInventory/main.cs
+++ PlaceFromInventory/main.cs
@@ -5,7 +5,7 @@ namespace PlaceFromInventory
 {
 	public class Main: Mod
 	{
-		internal const string version = "1.0.0";
+		internal const string version = "1.1.0";
 
 		internal static readonly ModConfig config = Config.tryLoad<ModConfig>();
 	}

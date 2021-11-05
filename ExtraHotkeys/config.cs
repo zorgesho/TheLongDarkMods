39ed10cdaec2f899a253fbef9a2502321c568c80
zorgesho <53096970+zorgesho@users.noreply.github.com>
2021-11-05 17:19:36 +0300

    validation for action list

--- ExtraHotkeys/config.cs
+++ ExtraHotkeys/config.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
+using System.Collections.Generic;
+
 using UnityEngine;
+
+using Common;
 using Common.Configuration;
 
 namespace ExtraHotkeys
@@ -8,8 +12,20 @@ namespace ExtraHotkeys
 	{
 		protected override void onLoad()
 		{
+			void _resetBind(Actions.InventoryAction action) => binds[action.id] = KeyCode.None;
+
 			if (binds.Count == 0)
-				Actions.actions.ForEach(action => binds[action.id] = KeyCode.None);
+			{
+				Actions.actions.ForEach(_resetBind);
+			}
+			else
+			{
+				// updating binds in case action list is changed
+				var listToRemove = binds.Where(bind => !Actions.actions.Exists(action => action.id == bind.Key)).ToList();
+				listToRemove.ForEach(bind => binds.Remove(bind.Key));
+
+				Actions.actions.Where(action => !binds.ContainsKey(action.id)).forEach(_resetBind);
+			}
 		}
 
 		public readonly Dictionary<string, KeyCode> binds = new();
